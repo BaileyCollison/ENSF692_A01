23bcc365fded8fcdc00428902724e57d87c122b1
BaileyCollison <138839703+BaileyCollison@users.noreply.github.com>
2024-05-09 09:24:10 -0600

    Assignment 1 push

---
+++ assignment1code.py
@@ -0,0 +1,12 @@
+# ENSF 692 Spring 2024
+# Assignment 1 Starter Code
+# Bailey Collison
+
+studentName = "Bailey Collison"  # Change the contents of this variable to your name (leave the quotation marks).
+
+print("Hello, World!")
+print("My name is " + studentName + " and I am a student in ENSF 692.")
+
+# print your best skill here
+
+print("My best skill is building things!")
